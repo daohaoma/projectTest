2a60405b5b27fe52492c0e70374576123996e352
QIAOFANG\jianhua.zhu <m18375326020_1@163.com>
2018-04-28 14:34:49 +0800

    feat: add elementui

--- vue_element/src/router/index.js
+++ vue_element/src/router/index.js
@@ -2,14 +2,15 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+import Test from '../pages/test'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'test',
+      component: Test
     }
   ]
 })
